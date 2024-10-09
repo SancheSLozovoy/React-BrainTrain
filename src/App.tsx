4ec1032d3766250ebdaf91bc5c4f672db9c0002a
SanchesLozovoy <alex.lozovoy05@bk.ru>
2024-10-09 16:41:27 +0300

    feat: add base struct for settings page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div></div>
-  );
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Settings from './pages/settings/Settings';
+import Game from './pages/game/Game';
+ 
+const App: React.FunctionComponent = () => {
+  return ( 
+    <Router>
+      <Routes>
+        <Route path='/' element={<Settings/>}/>
+        <Route path='/game' element={<Game/>}/>
+      </Routes>
+    </Router>
+   );
 }
-
+ 
 export default App;
--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,5 +1,44 @@
+import { Link } from 'react-router-dom';
+import './Settings.css';
+
 const Settings: React.FunctionComponent = () => {
-    return ( <div></div> );
+    return (
+        <section className="settings">
+
+            <div className="statistic__container">
+                <h1 className="statistic__container-title">Статистика</h1>
+                <span className="statistic__container-date"></span>
+                <span className="statistic__container-score"></span>
+            </div>
+
+            <div className="settings__inner">
+                <h1 className="settings__inner-title">Настройки</h1>
+                <div className="settings__inner-chars">
+
+                </div>
+                <label htmlFor="input">
+                    Минимальное число
+                    <input defaultValue={10} type="number"></input>
+                </label>
+                <label htmlFor="input">
+                    Максимальное число
+                    <input defaultValue={100} type="number"></input>
+                </label>
+                <label htmlFor="input">
+                    Сложность
+                    <input defaultValue={1} type="number"></input>
+                </label>
+                <label htmlFor="input">
+                    Время
+                    <input defaultValue={60} type="number"></input>
+                </label>
+                <Link to="/game">
+                    <button className="settings__inner-button">Начать игру</button>
+                </Link>
+
+            </div>
+        </section>
+    );
 }
- 
+
 export default Settings;
