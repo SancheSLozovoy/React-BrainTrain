cb95a5704b876cdcc53d0a38098a5ae59bd34168
SanchesLozovoy <alex.lozovoy05@bk.ru>
2024-10-10 10:24:29 +0300

    feat: add setting functions

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -1,16 +1,50 @@
 import { Link } from 'react-router-dom';
 import './Settings.css';
 import { Operator, SettingsProps } from '../../types/types';
+import { useState } from 'react';
 
 const DEFAULT_OPERATORS: Operator[] = [
-    {symbol: '+', precedence: 1},
-    {symbol: '-', precedence: 1},
-    {symbol: '*', precedence: 2},
-    {symbol: '/', precedence: 2},
+    { symbol: '+', precedence: 1 },
+    { symbol: '-', precedence: 1 },
+    { symbol: '*', precedence: 2 },
+    { symbol: '/', precedence: 2 },
 ]
 
 
 const Settings: React.FunctionComponent = () => {
+    const [allowedOperators, setAllowedOperators] = useState<Operator[]>(DEFAULT_OPERATORS)
+    const [minNumber, setMinNumber] = useState<number>(10);
+    const [maxNumber, setMaxNumber] = useState<number>(100);
+    const [complexity, setComplexity] = useState<number>(3);
+    const [time, setTime] = useState<number>(60);
+
+    const handleChangeOperators = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value, checked } = e.target;
+        
+        const operator = DEFAULT_OPERATORS.find(op => op.symbol === value);
+
+        if (checked && operator) {
+            if (!allowedOperators.some(op => op.symbol === operator.symbol)) {
+                setAllowedOperators([...allowedOperators, operator]);
+                setComplexity(prev => prev + 1);
+            }
+        } else if (!checked && operator) {
+            setAllowedOperators(prev => prev.filter(op => op.symbol !== operator.symbol));
+            setComplexity(prev => prev - 1);
+        }
+    };
+
+    const handleStart = () => {
+        const settings: SettingsProps = {
+            allowedOperators,
+            minNumber,
+            maxNumber,
+            timeLimit: time,
+            complexity,
+        }
+        localStorage.setItem('gameSettings', JSON.stringify(settings))
+    }
+
     return (
         <section className="settings">
 
@@ -26,9 +60,11 @@ const Settings: React.FunctionComponent = () => {
                     <h2>Допустимые знаки</h2>
                     {DEFAULT_OPERATORS.map(operator => (
                         <label key={operator.symbol}>
-                            <input 
+                            <input
                                 type="checkbox"
-                                value={operator.symbol}     
+                                value={operator.symbol}
+                                checked={allowedOperators.some(op => op.symbol === operator.symbol)}
+                                onChange={handleChangeOperators}
                             />
                             {operator.symbol}
                         </label>
@@ -37,22 +73,43 @@ const Settings: React.FunctionComponent = () => {
                 </div>
                 <label htmlFor="input">
                     Минимальное число
-                    <input defaultValue={10} type="number"></input>
+                    <input
+                        defaultValue={minNumber}
+                        type="number"
+                        onChange={(e) => setMinNumber(Number(e.target.value))}
+                    >
+                    </input>
                 </label>
                 <label htmlFor="input">
                     Максимальное число
-                    <input defaultValue={100} type="number"></input>
+                    <input
+                        defaultValue={maxNumber} 
+                        type="number"
+                        onChange={(e) => setMaxNumber(Number(e.target.value))}
+                    >
+                    </input>
                 </label>
                 <label htmlFor="input">
                     Сложность
-                    <input defaultValue={1} type="number"></input>
+                    <input 
+                        defaultValue={complexity} 
+                        type="number"
+                        onChange={(e) => setComplexity(Number(e.target.value))}
+                        min={2}
+                    >
+                    </input>
                 </label>
                 <label htmlFor="input">
                     Время
-                    <input defaultValue={60} type="number"></input>
+                    <input 
+                        defaultValue={time} 
+                        type="number"
+                        onChange={(e) => setTime(Number(e.target.value))}
+                    >
+                    </input>
                 </label>
                 <Link to="/game">
-                    <button className="settings__inner-button">Начать игру</button>
+                    <button onClick={handleStart} className="settings__inner-button">Начать игру</button>
                 </Link>
 
             </div>
